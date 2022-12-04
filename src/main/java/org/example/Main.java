d9047a5099f7cb84d72ec9c22a72f5907c05a4b5
Rafiq Khan <rafiqkhan.pathan@gmail.com>
2022-12-04 15:00:41 +0800

    modified variable i and assigned to 100

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int i=1;
-        i=i+10;
-        i=i-5;
+        int i=100;i++;
         System.out.println("Value of variable:"+i);
 
     }
