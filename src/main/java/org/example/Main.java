944f351feca42ce3b9f2c9950a4f5b2b2e997fe8
Rafiq Khan <rafiqkhan.pathan@gmail.com>
2022-12-04 14:57:17 +0800

    test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int i=1;
+        i=i+10;
+        i=i-5;
+        System.out.println("Value of variable:"+i);
+
+    }
+}
