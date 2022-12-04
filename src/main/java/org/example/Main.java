683e89726889b9022bb1adbd22281ffe96466c7e
Rafiq Khan <rafiqkhan.pathan@gmail.com>
2022-12-04 15:02:41 +0800

    Lamodified variable i and assigned to 100

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int i=100;i++;
+        int i=100;i++; i++;
         System.out.println("Value of variable:"+i);
 
     }
